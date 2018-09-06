f86bbacd4b35b1bf8fd47f3cbd9816abddd13463
ESB <eb@eb2.co>
2018-09-06 11:42:32 -0500

    Enable optimizations

--- build_msvc.bat
+++ build_msvc.bat
@@ -1,15 +1,14 @@
 @echo off
 
+set PROJECT=ptimer
+set COMPILER_OPTIONS=-O2 -EHsc -nologo -W4 -WX -MD -permissive- -std:c++17
+set COMPILER_DEFINES=-D%PROJECT%_INTERNAL
+set LINKER_OPTIONS=-out:ptimer.exe -subsystem:console
+set LINKER_LIBRARIES=
+
 if not exist %~dp0build mkdir %~dp0build
 pushd %~dp0build
 
-set PROJECT=ptimer
-set COMPILER_OPTIONS=-nologo -EHsc -Z7 -W4 -WX -permissive- -std:c++17
-set COMPILER_DEFINES=-D%PROJECT%_INTERNAL
-set LINKER_OPTIONS=-out:ptimer.exe
-set LINKER_LIBRARIES=
-
-rem Build the project executable
-cl %COMPILER_OPTIONS% %COMPILER_DEFINES% %~dp0src/win32_%PROJECT%.cpp /link %LINKER_OPTIONS%
+ptime cl %COMPILER_OPTIONS% %COMPILER_DEFINES% %~dp0src\win32_%PROJECT%.cpp /link %LINKER_OPTIONS% %LINKER_LIBRARIES%
 
 popd
