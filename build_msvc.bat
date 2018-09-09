21f279977e26c573f152acf78e2d6a346aa6f8ec
Eric Barr <eb@eb2.co>
2018-09-09 16:06:56 -0500

    Handle spaces in paths to source files.

--- build_msvc.bat
+++ build_msvc.bat
@@ -6,9 +6,9 @@ set COMPILER_DEFINES=-D%PROJECT%_INTERNAL -DWIN32_LEAN_AND_MEAN
 set LINKER_OPTIONS=-out:ptimer.exe -subsystem:console -entry:main -nodefaultlib
 set LINKER_LIBRARIES=Kernel32.lib User32.lib
 
-if not exist %~dp0build mkdir %~dp0build
-pushd %~dp0build
+if not exist "%~dp0build" mkdir "%~dp0build"
+pushd "%~dp0build"
 
-ptime cl %COMPILER_OPTIONS% %COMPILER_DEFINES% %~dp0src\win32_%PROJECT%.cpp /link %LINKER_OPTIONS% %LINKER_LIBRARIES%
+ptime cl %COMPILER_OPTIONS% %COMPILER_DEFINES% ^"%~dp0src\win32_%PROJECT%.cpp^" /link %LINKER_OPTIONS% %LINKER_LIBRARIES%
 
 popd
